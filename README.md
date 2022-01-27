71ecd7689061d59ca67a1d2b7c49220ca93377d5
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-01-27 18:20:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# translate-bot
+
+A Telegram bot that translates English messages to Danish and German.
+
+## Run
+
+Set `api_token` and `project_id` in `config.json`.
+
+Save Google Translate credential in `credential.json`.
+
+Run `python main.py`.
---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credential.json'
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.markdown import escape_md
+from google.cloud import translate
+import json
+
+def load_config():
+    with open('config.json') as f:
+        o = json.load(f)
+    return o['api_token'], o['project_id']
+
+API_TOKEN, PROJECT_ID = load_config()
+
+def is_english(text: str):
+    parent = f'projects/{PROJECT_ID}/locations/global'
+    response = client.detect_language(request={
+        'parent': parent,
+        'content': text,
+        'mime_type': 'text/plain',
+    })
+    return response.languages[0].language_code == 'en'
+
+def translate_text(text: str, lang: str):
+    parent = f'projects/{PROJECT_ID}/locations/global'
+    response = client.translate_text(request={
+        'parent': parent,
+        'contents': [text],
+        'mime_type': 'text/plain',
+        'source_language_code': 'en-US',
+        'target_language_code': lang,
+    })
+    return response.translations[0].translated_text
+
+client = translate.TranslationServiceClient()
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    text = message.text
+    if is_english(text):
+        await message.reply(escape_md(f'''🇩🇰 {translate_text(text, lang='da')}
+🇩🇪 {translate_text(text, lang='de')}'''), parse_mode='MarkdownV2')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
